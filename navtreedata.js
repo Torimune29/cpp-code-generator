b2fd2ce275b32a21b084cd9c5332f9a0d102861a
Torimune29 <Torimune29@users.noreply.github.com>
2022-06-21 03:51:06 +0000

    deploy: a1d7b78a16a32b07c2404db5999aa59bfe1f3da9

--- navtreedata.js
+++ navtreedata.js
@@ -25,7 +25,16 @@
 var NAVTREE =
 [
   [ "cppcodegen", "index.html", [
-    [ "Features(WIP)", "index.html#autotoc_md1", null ],
+    [ "Features", "index.html#autotoc_md1", null ],
+    [ "Example", "index.html#autotoc_md2", null ],
+    [ "How to use", "index.html#autotoc_md3", [
+      [ "How to install", "index.html#autotoc_md4", null ],
+      [ "How to library", "index.html#autotoc_md5", [
+        [ "Snippet", "index.html#autotoc_md6", null ],
+        [ "Block and Class", "index.html#autotoc_md7", null ]
+      ] ],
+      [ "Indenting", "index.html#autotoc_md8", null ]
+    ] ],
     [ "Namespaces", "namespaces.html", [
       [ "Namespace List", "namespaces.html", "namespaces_dup" ],
       [ "Namespace Members", "namespacemembers.html", [
